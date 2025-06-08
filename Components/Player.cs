83a19de21a2e6f5f8f694de6c9993c1635261445
Quy's Multiverse <chanhquy01@gmail.com>
2025-06-08 20:31:23 +0700

    Add Base Game Module

---
+++ Components/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPG.Components
+{
+    internal class Player (Room startingRoom)
+    {
+        public Room CurrentRoom { get; set; } = startingRoom;
+    }
+}
---
+++ Components/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPG.Components
+{
+    internal class Room(string name, string description)
+    {
+        public string Name { get; } = name;
+        public string Description { get; } = description;
+        public Dictionary<string, Room> Exits { get; } = [];
+    }
+}
